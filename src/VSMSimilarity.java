804ce00e45681072d1975d6e085aae1b2ca3e517
Joonas Nissinen <joonas.nissinen@aalto.fi>
2014-03-25 09:16:02 +0200

    VSM Similarity with sublinear weighting.

--- src/VSMSimilarity.java
+++ src/VSMSimilarity.java
@@ -1,46 +1,26 @@
-import org.apache.lucene.index.FieldInvertState;
-import org.apache.lucene.search.similarities.TFIDFSimilarity;
-import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.search.similarities.DefaultSimilarity;
 
-public class VSMSimilarity extends TFIDFSimilarity {
+public class VSMSimilarity extends DefaultSimilarity {
+
+    public final boolean sublinear = true;
 
     public VSMSimilarity() {
 
     }
 
+    // Term frequency (tf) in a document is a measure of how often a term appears in the document
+    // Using sub-linear term frequency weighting to avoid crappy results.
     @Override
-    public float coord(int i, int i2) {
-        return 0;
+    public float tf(int freq) {
+        if (sublinear) {
+            return (freq > 0) ? (1 + (float)Math.log(freq)) : 0;
+        }
+        return freq;
     }
 
+    // A score factor based on term overlap with the query i.e. number of terms found in the query
     @Override
-    public float queryNorm(float v) {
-        return 0;
+    public float coord(int overlap, int maxOverlap) {
+        return 1;
     }
-
-    @Override
-    public float tf(float v) {
-        return 0;
-    }
-
-    @Override
-    public float idf(long l, long l2) {
-        return 0;
-    }
-
-    @Override
-    public float lengthNorm(FieldInvertState fieldInvertState) {
-        return 0;
-    }
-
-    @Override
-    public float sloppyFreq(int i) {
-        return 0;
-    }
-
-    @Override
-    public float scorePayload(int i, int i2, int i3, BytesRef bytesRef) {
-        return 0;
-    }
-
 }
